29e9e3527f4cc389bc4d820bb08f89d057bc53c3
xKirtle <xkirtle26@gmail.com>
2020-12-08 05:02:34 +0000

    Day6 Challenge Part2

--- DailyChallenges/Day6.cs
+++ DailyChallenges/Day6.cs
@@ -40,5 +40,32 @@ namespace AdventOfCode
 
             Console.WriteLine($"The sum of questions everyone answered yes is {sumOfTrueQuestions}");
         }
+
+        public static void Part2()
+        {
+            string[] inputs = handleInput();
+            int sumOfTrueQuestions = 0;
+            
+            foreach (string input in inputs)
+            {
+                string[] groupInputs = input.Split(new string[] {" ", "\r\n"}, StringSplitOptions.None);
+                
+                //a 97 - z 122
+                bool[] questionsSelected = new bool[26];
+                
+                for (int i = 0; i < groupInputs.Length; i++)
+                for (int j = 0; j < groupInputs[i].Length; j++)
+                    questionsSelected[groupInputs[i][j] % 123 - 97] = true;
+                
+                for (int i = 0; i < questionsSelected.Length; i++)
+                {
+                    char arrChar = (char) (i + 97);
+                    if (groupInputs.Count(x => x.Contains(arrChar.ToString())) == groupInputs.Length)
+                        sumOfTrueQuestions++;
+                }
+            }
+            
+            Console.WriteLine($"The sum of questions everyone in the same group answered yes is {sumOfTrueQuestions}");
+        }
     }
 }
